ce6d563bad938d37a1d350118eb363629b993e3d
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-16 15:30:39 +0530

    Button assignment 1

---
+++ react-practice copy/trycontextapi/src/Buttons/Buttons4.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect,useState} from "react";
+import './Buttonscss.css'
+
+const Buttons4=()=>{
+
+    const [buttons,setbuttons]=useState([])
+
+    const [states,setStates]=useState(["false"])
+
+    const [count,setcount]=useState(0);
+
+    const addbuttons=()=>{
+        const newarr=[];
+        for(let i=0;i<9;i++){
+            newarr.push({id:i,text:`Btn${i}`});
+        }
+        setbuttons(newarr);
+    }
+
+    const handleclick=(e)=>{
+        console.log(e.target)
+        const button=e.target;
+        if(count<4){
+            if(button.dataset.isclicked==="false"){
+                button.dataset.isclicked="true";
+                button.style.backgroundColor="beige";
+                button.style.color="black";
+                setcount((prev)=>prev+1);
+            }
+        }
+    }
+
+    useEffect(()=>{
+        addbuttons()
+    },[])
+
+
+    return (
+        <div className="main">
+            <div className="diving">
+                {
+                    buttons.map((btnobj)=>{
+                        return (
+                            <button key={btnobj.id} className="buttonInsideGrid" onClick={handleclick} data-isclicked="false">
+                                {btnobj.text} 
+                            </button>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Buttons4
