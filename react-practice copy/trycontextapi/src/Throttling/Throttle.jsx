419539b18f7645a0d030722dceca95a1036ee482
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-24 17:56:47 +0530

    throttling

---
+++ react-practice copy/trycontextapi/src/Throttling/Throttle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Throttle=()=>{
+
+
+
+    return (
+        <div style={{display:"flex",justifyContent:"center",alignItems:"center",backgroundColor:"blue"}}>
+            <div>
+                <button style={{backgroundColor:"beige",color:"black",padding:"1rem"}}>Click me</button>
+            </div>
+        </div>
+    )
+}
