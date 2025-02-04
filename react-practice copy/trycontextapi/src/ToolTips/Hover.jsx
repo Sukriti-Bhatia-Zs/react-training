9baed317237de7b507123a04ce9883f492c26e4d
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-02-04 14:35:31 +0530

    made a commit

--- react-practice copy/trycontextapi/src/ToolTips/Hover.jsx
+++ react-practice copy/trycontextapi/src/ToolTips/Hover.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Tooltip from "./Tooltip";
 
 const Hover = ({ text, alignment }) => {
+  let x = 4;
+  console.log(x);
+  
     return (
       <Tooltip text={text} alignment={alignment}>
         <span>Hover over me</span>
